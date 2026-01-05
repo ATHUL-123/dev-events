0aad9c7b95022c087031ee1e2a2f7518a6c908db
Athul <athulkrishnatv702@gmail.com>
2026-01-05 10:44:54 +0530

    Implement Database models

---
+++ lib/constants.ts
@@ -0,0 +1,59 @@
+export type  EventItem = {
+    title: string;
+    image: string;
+    slug:string;
+    location:string;
+    date:string;
+    time:string;
+}
+
+export const events: EventItem[] = [
+  {
+    image: '/images/event1.png',
+    slug: 'event-1',
+    location: 'location-1',
+    date: 'date-1',
+    time: 'time-1',
+    title: 'Event 1',
+  },
+  {
+    image: '/images/event2.png',
+    slug: 'event-2',
+    location: 'location-2',
+    date: 'date-2',
+    time: 'time-2',
+    title: 'Event 2',
+  },
+  {
+    image: '/images/event3.png',
+    slug: 'event-3',
+    location: 'location-3',
+    date: 'date-3',
+    time: 'time-3',
+    title: 'Event 3',
+  },
+  {
+    image: '/images/event4.png',
+    slug: 'event-4',
+    location: 'location-4',
+    date: 'date-4',
+    time: 'time-4',
+    title: 'Event 4',
+  },
+  {
+    image: '/images/event5.png',
+    slug: 'event-5',
+    location: 'location-5',
+    date: 'date-5',
+    time: 'time-5',
+    title: 'Event 5',
+  },
+  {
+    image: '/images/event6.png',
+    slug: 'event-6',
+    location: 'location-6',
+    date: 'date-6',
+    time: 'time-6',
+    title: 'Event 6',
+  }
+]
